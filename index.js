2a7cf0d1da3bb4698fcf209d334cf04aa3d62516
saurabhsingh06 <saurabh.singh@vnnogile.in>
2023-03-25 15:42:13 +0530

    Server start

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express");
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.status(200).json({
+        status: true,
+        message: "Server is active and ready to serve :)"
+    })
+});
+
 app.listen(5000, () => {
     console.log("Server is listening at port 5000")
 })
